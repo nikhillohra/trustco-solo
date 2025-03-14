0bd55f541136c0bfed9148141ee0958ef048a579
nikhillohra <nikhillohra99@gmail.com>
2025-03-14 19:46:05 +0530

    [#TR-5] - Trusted Brand Section

--- js/script.js
+++ js/script.js
@@ -2,7 +2,15 @@ document.addEventListener("DOMContentLoaded", function () {
     const hamburger = document.getElementById("hamburger");
     const mobileMenu = document.getElementById("mobileMenu");
 
+    // Toggle class on click
     hamburger.addEventListener("click", function () {
         mobileMenu.classList.toggle("active");
+    });
+
+    // Close menu when clicking outside
+    document.addEventListener("click", function (event) {
+        if (!hamburger.contains(event.target) && !mobileMenu.contains(event.target)) {
+            mobileMenu.classList.remove("active");
+        }
     });
 });
